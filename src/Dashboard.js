bb9a285d78be9aff3fc8653831e9d0686eef2fd6
kundurthivenkatakishore <kundurthivenkatakishore@gmail.com>
2022-03-05 17:39:41 +0530

    intial-files

---
+++ src/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+function Dashboard(){
+    return(
+        <div className='dashboard'>
+            <h1>React CRUD Operations</h1>
+        </div>
+    )
+}
+export default Dashboard;
